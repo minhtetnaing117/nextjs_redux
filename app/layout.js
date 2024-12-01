8693743562e798ac9009effe2fe4ab86c14e0a0d
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-12-01 13:49:37 +0630

    add layout js

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import ReduxProvider from '@/provider/redux/ReduxProvider'
+
+import "./styles/globals.css";
+
+const inter = Inter({ subsets: ['latin']})
+
+export const metadata = {
+  title : 'Create Next app',
+  description: ' Generatedby create next app',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <ReduxProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+      
+    </ReduxProvider>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Counter } from "./components/counter/Counter";
+
+export default function Home() {
+  return (
+    <>
+      <div className="w-54 h-54 bg-red-500">
+
+      </div>
+    </>
+  );
+}
+
